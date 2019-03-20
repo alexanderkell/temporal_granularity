e3149dcf537d5cf1d0daeb0422170b3130fab96c
Alexander Kell <alexander@kell.es>
2019-03-20 14:43:20 +0000

    added wind and pv data for GB

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
+import pandas as pd
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import os
 
+# os.path.join(os.path.dirname(__file__))
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
@@ -15,16 +13,16 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
+    wind = pd.read_csv(input_filepath)
+    print(wind.head())
+
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+    project_dir = Path("__file__").resolve().parents[2]
+    
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    main('{}/data/raw/ninja_wind_country_GB_current-merra-2_corrected (2).csv'.format(project_dir), '{}/data/processed/wind_processed.csv'.format(project_dir))
