d202f2a567529b63d61bd6e48efdfd3d0d6f164f
Alexander Kell <alexander@kell.es>
2019-03-28 08:32:49 +0000

    started working on comparing methods to LDC

--- src/data/make_dataset_pv.py
+++ src/data/make_dataset_pv.py
@@ -13,8 +13,14 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
-    wind = pd.read_csv(input_filepath)
-    print(wind.head())
+    solar_data = pd.read_csv(input_filepath.format(project_dir), skiprows=2)
+    
+    solar_global = solar_data[['time','GB_TOTAL']]
+
+    solar_global = solar_global.rename(columns={"GB_TOTAL":"capacity_factor","time":"datetime"})
+    solar_global.datetime = pd.to_datetime(solar_global.datetime)
+
+    solar_global.to_csv(output_filepath)
 
 
 if __name__ == '__main__':
@@ -22,7 +28,7 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
-    project_dir = Path("__file__").resolve().parents[2]
+    project_dir = Path("__file__").resolve().parents[1]
     
 
-    main('{}/data/raw/ninja_wind_country_GB_current-merra-2_corrected (2).csv'.format(project_dir), '{}/data/processed/wind_processed.csv'.format(project_dir))
+    main('{}/temporal_granularity/data/raw/resources/ninja_pv_country_GB_sarah_nuts-2_corrected.csv'.format(project_dir), '{}/temporal_granularity/data/processed/resources/pv_processed.csv'.format(project_dir))
---
+++ src/data/make_dataset_wind.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import logging
+import pandas as pd
+from pathlib import Path
+import os
+
+# os.path.join(os.path.dirname(__file__))
+
+def main(input_filepath, output_filepath):
+    """ Runs data processing scripts to turn raw data from (../raw) into
+        cleaned data ready to be analyzed (saved in ../processed).
+    """
+    logger = logging.getLogger(__name__)
+    logger.info('making final data set from raw data')
+
+    wind_dat = pd.read_csv(input_filepath.format(project_dir), skiprows=2)
+    
+    wind_off_onshore = wind_dat.drop('national', axis=1)
+
+    wind_off_onshore.time=pd.to_datetime(wind_off_onshore.time)
+
+    wind_off_onshore.to_csv(output_filepath)
+
+
+if __name__ == '__main__':
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    # not used in this stub but often useful for finding various files
+    project_dir = Path("__file__").resolve().parents[1]
+    
+
+    main('{}/temporal_granularity/data/raw/resources/ninja_wind_country_GB_current-merra-2_corrected (2).csv'.format(project_dir), '{}/temporal_granularity/data/processed/resources/wind_processed.csv'.format(project_dir))
