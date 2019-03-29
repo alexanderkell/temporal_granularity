37ca22b51ca562a2c56168cb780ab77f9d7dc4ef
Alexander Kell <alexander@kell.es>
2019-03-29 16:18:42 +0000

    finished visualisation of centroids and medoids and seasonal

--- src/models/approximations.py
+++ src/models/approximations.py
@@ -12,10 +12,10 @@ from sklearn.metrics import pairwise_distances_argmin_min
 
 import logging
 
-class approximate_data:
+class ApproximateData:
 
     def __init__(self, data_dir, data_type):
-        self.data = self.import_data(data_dir)
+        self.data = self._import_data(data_dir)
         self.data_type = data_type
         
 
@@ -37,12 +37,14 @@ class approximate_data:
 
         return average_day_df
 
-    def cluster_on_kmeans(self, n_clusters):
+    def kmeans_centroids(self, n_clusters):
 
-        each_day = self.long_to_wide_data()
+        each_day = self._long_to_wide_data()
 
-        kmeans = self.get_kmeans(each_day, n_clusters)
+        kmeans = self._get_kmeans(each_day, n_clusters)
         y_kmeans = kmeans.predict(each_day)
+        y_kmeans_df = pd.DataFrame(y_kmeans)
+        y_kmeans_df = y_kmeans_df.rename(columns={y_kmeans_df.columns[0]:'cluster'})
 
         cluster_centres = kmeans.cluster_centers_
         cluster_centres_df = pd.DataFrame(cluster_centres)
@@ -54,28 +56,77 @@ class approximate_data:
               12,        13,        14,        15,        16,        17,
               18,        19,        20,        21,        22,        23,])
         
-        print(centres_df_long)
-        return centres_df_long
+        centres_df_long = centres_df_long.rename(columns={"value":"capacity_factor"})
+
+        return centres_df_long, y_kmeans_df
 
 
     def cluster_medoids(self, n_clusters):
 
-        each_day = self.long_to_wide_data()
+        each_day = self._long_to_wide_data()
 
-        k_means = self.get_kmeans(each_day, n_clusters)
+        k_means = self._get_kmeans(each_day, n_clusters)
         y_kmeans = k_means.predict(each_day)
+
+        y_kmeans_df = pd.DataFrame(y_kmeans)
+        y_kmeans_df = y_kmeans_df.rename(columns={y_kmeans_df.columns[0]:'cluster'})
 
         each_day_w_kmeans = each_day.copy()
         each_day_w_kmeans['cluster'] = y_kmeans
 
         closest_data_points, _ = pairwise_distances_argmin_min(k_means.cluster_centers_, each_day)
 
-        medoids = self.get_medoids(closest_data_points, each_day_w_kmeans)
+        medoids = self._get_medoids(closest_data_points, each_day_w_kmeans)
             
 
-        return medoids
+        return medoids, y_kmeans_df
 
-    def import_data(self, data_dir):
+    
+    def get_load_duration_curve(self, data=None, clusters=None):
+        if data is None:
+            data_each_year = self.data.copy()
+            data_each_year= self.data.reset_index()
+            data_each_year['year'] = data_each_year.datetime.dt.year
+            data_each_year = data_each_year.groupby('year').apply(lambda x: x.sort_values('capacity_factor', ascending=False).reset_index().reset_index())
+        elif clusters is None:
+            data_each_year = self._scale_data_to_seasons(data)
+            data_each_year = data_each_year.sort_values('capacity_factor', ascending=False).reset_index().reset_index()
+
+        else:
+            data_each_year = self._scale_data_to_clusters(data, clusters)
+            data_each_year = data_each_year.sort_values('capacity_factor', ascending=False).reset_index()
+            data_each_year = data_each_year.drop('level_0', axis=1).reset_index()
+
+        data_each_year = data_each_year.rename(columns={"level_0":"index_for_year"})
+        return data_each_year
+
+    def _scale_data_to_seasons(self, data):
+        bins = [0, 70, 163, 245, 358, 366]
+        days_per_season = [x - bins[i - 1] for i, x in enumerate(bins)][1:]
+        days_per_season[0] = days_per_season[0]+days_per_season[-1]
+        del days_per_season[-1]
+        labels=['Winter', 'Spring', 'Summer', 'Autumn']
+        season_scaling = {'season':labels, 'days':days_per_season}
+        season_scaling_df = pd.DataFrame(season_scaling)
+        season_scaling_df.head()
+
+
+        average_day_scaled = data.merge(season_scaling_df, on='season')
+
+        average_day_scaled = average_day_scaled.reindex(average_day_scaled.index.repeat(average_day_scaled.days))
+        
+        return average_day_scaled
+
+    def _scale_data_to_clusters(self, data, clusters):
+        clusters = clusters.reset_index()
+        cluster_weights = clusters.groupby('cluster').count()
+        scaled_data = data.merge(cluster_weights, on='cluster') #[['cluster','capacity_factor','year']]
+        scaled_data['index'] = scaled_data['index']/37
+        scaled_data = scaled_data.reindex(scaled_data.index.repeat(scaled_data['index']))
+
+        return scaled_data
+
+    def _import_data(self, data_dir):
         data = pd.read_csv(data_dir).drop('Unnamed: 0', axis=1)
 
 
@@ -88,18 +139,18 @@ class approximate_data:
         return data
 
         
-    def get_medoids(self, closest_data_points, each_day_w_kmeans):
+    def _get_medoids(self, closest_data_points, each_day_w_kmeans):
         medoids = pd.DataFrame([])
         for index in closest_data_points:
             closest_datapoint = pd.DataFrame(each_day_w_kmeans.iloc[index,:])
             closest_datapoint['cluster']=closest_datapoint.iloc[24,0]
-            closest_datapoint = closest_datapoint.rename(columns={ closest_datapoint.columns[0]: "value" })
+            closest_datapoint = closest_datapoint.rename(columns={ closest_datapoint.columns[0]: "capacity_factor" })
             closest_datapoint=closest_datapoint.drop('cluster', axis=0)
             medoids = medoids.append(closest_datapoint)
         return medoids
 
 
-    def long_to_wide_data(self):
+    def _long_to_wide_data(self):
         date_hour = self.data.copy()
 
 
@@ -108,7 +159,7 @@ class approximate_data:
 
         return each_day
 
-    def get_kmeans(self, each_day, n_clusters):
+    def _get_kmeans(self, each_day, n_clusters):
         kmeans = KMeans(n_clusters=n_clusters)
         kmeans.fit(each_day)
         return kmeans
@@ -121,7 +172,9 @@ if __name__ == '__main__':
     # not used in this stub but often useful for finding various files
     project_dir = Path("__file__").resolve().parents[1]
 
-    data_manipulator = approximate_data('{}/temporal_granularity/data/processed/resources/onshore_processed.csv'.format(project_dir), 'onshore')
+    data_manipulator = ApproximateData('{}/temporal_granularity/data/processed/resources/onshore_processed.csv'.format(project_dir), 'onshore')
     # data_manipulator.average_by_season()
-    # data_manipulator.cluster_on_kmeans(4)
-    data_manipulator.cluster_medoids(4)
+    data_manipulator.kmeans_centroids(4)
+    # data_manipulator.cluster_medoids(4)
+    # print(data_manipulator.get_load_duration_curve())
+    
--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,7 +1,64 @@
-import pandas as pd
+import sys
 from pathlib import Path
-import seaborn as sns
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-from sklearn.metrics import pairwise_distances_argmin_min
+project_dir = Path("__file__").resolve().parents[1]
+import os
+sys.path.insert(0, '{}/temporal_granularity/'.format(project_dir))
+project_dir = Path("__file__").resolve().parents[1]
 
+
+from src.models.approximations import ApproximateData
+import pandas as pd
+pd.set_option('display.max_rows', 500)
+import seaborn as sns
+import matplotlib.pyplot as plt
+from itertools import cycle
+
+def plot_orginal_and_approximated_ldc(data_dir, approximation_method, n_clusters, data_type):
+
+    data_manipulator = ApproximateData(data_dir, data_type)
+    original = data_manipulator.get_load_duration_curve()
+
+    if approximation_method == 'medoids':
+        centres, y_kmeans = data_manipulator.cluster_medoids(n_clusters)
+        data = data_manipulator.get_load_duration_curve(centres, y_kmeans)
+    elif approximation_method == "centroids":
+        centres, y_kmeans = data_manipulator.kmeans_centroids(n_clusters)
+        data = data_manipulator.get_load_duration_curve(centres, y_kmeans)
+    elif approximation_method == "season_average":
+        centres = data_manipulator.average_by_season()
+        data = data_manipulator.get_load_duration_curve(centres)
+    else:
+        raise ValueError("approximation method can not equal {}, must be medoids, centroids or season_average".format(approximation_method))
+
+    fig, ax = plt.subplots()
+    sns.lineplot(data=original, hue='year', x='index_for_year', y='capacity_factor', ax=ax)
+    sns.lineplot(data=data, y='capacity_factor', x='index_for_year')
+    plt.xlabel("Hours at capacity factor (h)")
+    plt.ylabel("Capacity Factor")
+    plt.title("Original load duration curve for each year\n for {} and {} with {} clusters".format(data_type, approximation_method, n_clusters))
+
+
+    directory = "{}/temporal_granularity/src/visualization/figures/{}".format(project_dir, data_type)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    image_dir = "{}/{}-clusters-{}.png".format(directory, approximation_method, n_clusters)
+    plt.savefig(image_dir)
+
+    plt.close()
+
+
+
+if __name__ == "__main__":
+    onshore_data = '/Users/b1017579/Documents/PhD/Projects/14-temporal-granularity/temporal_granularity/data/processed/resources/onshore_processed.csv'
+    offshore_data = '/Users/b1017579/Documents/PhD/Projects/14-temporal-granularity/temporal_granularity/data/processed/resources/offshore_processed.csv'
+    pv_data = '/Users/b1017579/Documents/PhD/Projects/14-temporal-granularity/temporal_granularity/data/processed/resources/pv_processed.csv'
+
+    data = [onshore_data, offshore_data, pv_data, onshore_data, offshore_data, pv_data]
+    resources = ['onshore', 'offshore', 'photovoltaic','onshore', 'offshore', 'photovoltaic']
+    method = ['centroids', 'medoids']
+
+    for data, resource, method in zip(data, resources, cycle(method)):
+        for n_clusters in range(1,10):
+            plot_orginal_and_approximated_ldc(data, method, n_clusters, resource)
+
+        
