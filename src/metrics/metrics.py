dff5610f58d764bab77e81098742a5b732554bb2
Alexander Kell <alexander@kell.es>
2019-03-29 17:19:26 +0000

    begun working on metrics for assessment

---
+++ src/metrics/metrics.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+class metrics:
+
+    def __init__(self, original_ldc, approximated_ldc):
+        self.original_ldc = original_ldc
+        self.approximated_ldc = approximated_ldc
+
+
+    def ldc_nrmse(self):
+        pass
+
+    def ldc_rae(self):
+        pass
+
+    def short_term_fluctations_nrmse(self):
+        pass
+
+    def medium_term_fluctuations_rae(self):
+        pass
+
+    def correlation_error(self):
+        pass
+
+    def _approximate_ldc(self):
+        pass
+    
+if __name__ == "__main__":
+    pass
